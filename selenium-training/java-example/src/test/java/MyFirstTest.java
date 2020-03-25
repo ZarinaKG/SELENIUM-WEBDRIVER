2560ffbeae4bad5fe3eba65168403465efe6f3c2
Zarina Karymbaeva <zkarymbaeva@gmail.com>
2020-03-26 00:07:56 +0100

    Revert "test3"
    This reverts commit 7b26b8df98e41cfd70aa4a5684cce981dfc58163.

--- selenium-training/java-example/src/test/java/MyFirstTest.java
+++ selenium-training/java-example/src/test/java/MyFirstTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
@@ -18,15 +16,13 @@ public class MyFirstTest {
     public void start(){
         System.setProperty("webdriver.chrome.driver","C:\\Tools\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver,20);
+        wait = new WebDriverWait(driver,10);
     }
 
     @Test
 
     public void MyFirstTest(){
         driver.get("http:\\www.google.com");
-        driver.findElement(By.name("fakebox-microphone")).click();
         driver.findElement(By.name("q")).sendKeys("Webdriver");
         driver.findElement(By.name("btnG")).click();
         wait.until(titleIs("webdriver -Google"));
